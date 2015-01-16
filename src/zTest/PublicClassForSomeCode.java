ff2c13384eb31f974c1641b19a956f83a1129411
darioush <darioush.jalali@gmail.com>
2015-01-16 13:01:40 -0800

    initial commit

---
+++ src/zTest/PublicClassForSomeCode.java
@@ -0,0 +1,8 @@
+package zTest;
+
+
+public class PublicClassForSomeCode {
+	public void someMethod() {
+		System.out.println("Hello world");
+	}
+}
---
+++ test/zTest/SomeTests.java
@@ -0,0 +1,14 @@
+package zTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeTests {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
